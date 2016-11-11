87c7b490146ddafef4cecfc46473aba3ed8cf3d1
Michael Fan <mchl_fan@yahoo.com>
2016-11-11 08:04:38 -0500

    Create Problem8.java

---
+++ src/Problem8.java
@@ -0,0 +1,24 @@
+
+public class Problem8 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner("");
+	}
+	
+	public static String bruteForce(int[] number) {
+		String toReturn = "";
+		long biggestSoFar = 0L;
+		for (int k = 13-1; k < number.length; k++) {
+			long product;
+			for (int j = k; j >= 0; j--) {
+				
+			}
+		}
+	}
+
+	//as expected, more efficient than
+	public static String findAnswer(int[] number) {
+
+	}
+
+}
