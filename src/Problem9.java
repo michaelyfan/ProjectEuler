b96cea12990eb5f90e540c49884edef2d68372bf
michael.fan <mchl_fan@yahoo.com>
2016-11-20 00:52:31 -0500

    Add Problem9.java

---
+++ src/Problem9.java
@@ -0,0 +1,23 @@
+
+public class Problem9 {
+
+	public static void main(String[] args) {
+		System.out.println(bruteForce());
+	}
+	
+	//bruteforce method
+	//tolerably quick for the values of the problem
+	public static int bruteForce() {
+		for (int c = 1; c <= 1000; c++) {
+			for (int b = 1; b <= c; b++) {
+				for (int a = 1; a <= b; a++) {
+					if (a*a + b*b == c*c && a+b+c == 1000) {
+						return a*b*c;
+					}
+				}
+			}
+		}
+		return -1;
+	}
+	
+}
